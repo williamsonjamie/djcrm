84790fc85795b6afcb53eed6912b684a06d2f133
jajwilliamson <jajwilliamson@hotmail.com>
2021-03-25 13:22:31 +0000

    TailwindCSS1

--- leads/views.py
+++ leads/views.py
@@ -43,22 +43,10 @@ def lead_update(request, pk):
     }
     return render(request, "leads/lead_update.html", context)
 
-# def lead_update(request, pk):
-#     lead = Lead.objects.get(id=pk)
-#     form = LeadModelForm()
-#     if request.method == "POST":
-#         form = LeadForm(request.POST)
-#         if form.is_valid():
-#             first_name = form.cleaned_data["first_name"]
-#             last_name = form.cleaned_data["last_name"]
-#             age = form.cleaned_data["age"]
-#             lead.first_name = first_name
-#             lead.last_name = last_name
-#             lead.age = age
-#             lead.save()
-#             return redirect("/leads/")
-#     context = {
-#         "form" : form,
-#         "lead" : lead
-#     }
-#     return render(request, "leads/lead_update.html", context)
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads/")
+
+def landing_page(request):
+    return render(request,"landing.html")
